a2adef94716500360484f8bf5bd79ae87783b2dd
Artem Tomshin <tweaker3@ya.ru>
2020-10-25 02:03:41 +0800

    Добавлен спящий режим

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ const exec = require('child_process').exec;
 
 const app = express()
 
-app.get('/sleep', (request, response) => {
-    exec('pmset displaysleepnow',
-        (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-
+/**
+ * Выключить экран
+ */
+app.get('/displaysleepnow', (request, response) => {
+    pmset('displaysleepnow')
     response.status(200).send('ok')
 });
+
+/**
+ * Спящий режим
+ */
+app.get('/sleepnow', (request, response) => {
+    pmset('sleepnow')
+    response.status(200).send('ok')
+});
+
+function pmset(cmd) {
+    exec('pmset ' + cmd,
+    (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    });
+}
 
 app.use((err, request, response, next) => {
     console.log(err);
